0459436a3fae825879bb0b5ac547a4d07a4aa0db
Brendan A R Sechter <b-sechter@unext.jp>
2018-04-13 12:02:45 +0900

    Reorganized project.

---
+++ python.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# Use framework version of python instead of pipenv (virtualenv) copy.
+# -(which)-> python3 -(readlink)-> python3.X -(readlink)-> framework python3.X
+PYTHON=$(readlink $(readlink $(which python3)))
+export PYTHONHOME=$(pipenv --venv)
+exec $PYTHON $1
+
