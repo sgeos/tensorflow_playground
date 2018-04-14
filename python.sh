bd3f6fb78475cea0dba834b10f18a3cffd951844
Brendan A R Sechter <sgeos@hotmail.com>
2018-04-14 11:40:21 +0900

    Fixed bug in python.sh

--- python.sh
+++ python.sh
@@ -3,6 +3,6 @@
 # Use framework version of python instead of pipenv (virtualenv) copy.
 # -(which)-> python3 -(readlink)-> python3.X -(readlink)-> framework python3.X
 PYTHON=$(readlink $(readlink $(which python3)))
-export PYTHONHOME=$(pipenv --venv)
+export PYTHONHOME=$(python3 -m pipenv --venv)
 exec $PYTHON $1
 
